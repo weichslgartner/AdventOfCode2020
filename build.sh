a158fa30f83a2b3ed7c1bcc5ae7c056d10aa0414
weichslgartner <weichslgartner@gmail.com>
2020-12-01 19:48:06 +0100

    clean_up

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
-mkdir -p build && cd build  && 
-conan  install .. --profile clang_profile --build=missing  && 
+#!/bin/bash
+mkdir -p build && cd build   
+if [ ! 'profile show clang_profile' ] ; then
+       conan profile new clang_profile --detect &&
+       conan profile update settings.compiler=clang clang_profile  && 
+       conan profile update settings.compiler.version=10 clang_profile  &&
+       conan profile update settings.compiler.libcxx=libstdc++11 clang_profile
+fi
+conan  install .. --profile clang_profile --build=missing   
 CC=clang-11 CXX=clang++-11  cmake -DENABLE_CLANG_TIDY=True -DENABLE_CPPCHECK=True ..  && 
 cmake --build .  
--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,62 +1,66 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <unordered_set>
-#include <string_view>
-#include <optional>
 #include <fmt/printf.h>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <span>
+#include <sstream>
+#include <unordered_set>
 
+using numb_type = unsigned;
 
-std::unordered_set<long long> parse_list(std::string const & file_name){
-    std::unordered_set<long long> numbers;
-    std::ifstream infile(file_name);
-    std::stringstream ss{};
-    std::string token;
-    if(infile.is_open()){
-        ss << infile.rdbuf();
-        while(ss >> token){
-            numbers.insert(static_cast<long long>(std::stoi(token)));
-        }
-    } else {
-      fmt::print(stderr, "Cannot open File {}!", file_name);
+std::unordered_set<numb_type> parse_list(std::string const &file_name) {
+  std::unordered_set<numb_type> numbers;
+  std::ifstream infile(file_name);
+  std::stringstream ss{};
+  std::string token;
+  if (infile.is_open()) {
+    ss << infile.rdbuf();
+    while (ss >> token) {
+      numbers.insert(std::stoul(token));
     }
-    return numbers;
+  } else {
+    fmt::print(stderr, "Cannot open File {}!", file_name);
+  }
+  return numbers;
 }
 
-std::optional<long long> part1(std::unordered_set<long long> &numbers, long long sum){
-    for(auto numb  : numbers){
-        auto need = sum - numb;
-        if(numbers.contains(need)){
-            return need*numb;
-        }
+std::optional<unsigned> part1(std::unordered_set<numb_type> &numbers,
+                              numb_type sum) {
+  for (auto numb : numbers) {
+    auto need = sum - numb;
+    if (numbers.contains(need)) {
+      return need * numb;
     }
-    return {};
+  }
+  return {};
 }
 
-std::optional<long long> part2(std::unordered_set<long long> &numbers, long long sum){
-    for(auto numb1  : numbers){
-        for(auto numb2 : numbers){
-            auto need = sum - numb1 - numb2;
-            if(numbers.contains(need)){ //numbers.find(need) != numbers.end()
-                 return need*numb1*numb2;
-            }
-        }
+std::optional<numb_type> part2(std::unordered_set<numb_type> &numbers,
+                               numb_type sum) {
+  for (auto numb1 : numbers) {
+    for (auto numb2 : numbers) {
+      auto need = sum - numb1 - numb2;
+      if (numbers.contains(need)) {
+        return need * numb1 * numb2;
+      }
     }
-    return {};
+  }
+  return {};
 }
 
 int main(int argc, char **argv) {
   auto const *file_name = "build/input/input_01.txt";
   if (argc == 2) {
-    file_name = argv[1];
+    std::span<char *> args{argv, 2};
+    file_name = args[1];
   }
+  constexpr auto target{2020U};
   auto numbers = parse_list(file_name);
-  auto res_part1 = part1(numbers, 2020);
+  auto res_part1 = part1(numbers, target);
   if (res_part1) {
     fmt::print("Part_1: {}\n", res_part1.value());
   }
-  auto res_part2 = part2(numbers, 2020);
+  auto res_part2 = part2(numbers, target);
   if (res_part1) {
     fmt::print("Part_2: {}\n", res_part2.value());
   }
