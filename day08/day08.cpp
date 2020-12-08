36d7c38adf785023abc233709a3cb10db0f66ad3
weichslgartner <weichslgartner@gmail.com>
2020-12-08 10:37:53 +0100

    day08 added return condition

--- day08/day08.cpp
+++ day08/day08.cpp
@@ -59,61 +59,62 @@ std::vector<Instruction> parse_input(char const *const file_name) {
 	return inst_mem;
 }
 
-
-std::optional<int> interprete(std::vector<Instruction> &inst_mem,bool part_1,unsigned max_count) {
+std::optional<int> interprete(std::vector<Instruction> &inst_mem, bool part_1, unsigned max_count) {
 	std::unordered_set<int> inst_hist;
-	int inst_pnt {0};
-	int acc_value {0};
-	auto inst_count {0U};
+	int inst_pnt { 0 };
+	int acc_value { 0 };
+	auto inst_count { 0U };
 	while (inst_pnt < inst_mem.size()) {
 		auto inst = inst_mem.at(inst_pnt);
 		if (inst_hist.contains(inst_pnt) and part_1) {
-			return acc_value;
+			if(part_1){
+				return acc_value;
+			}else{
+				return {};
+			}
 		}
-		if(inst_count > max_count) {
+		if (inst_count > max_count) {
 			return {};
 		}
 		inst_count++;
 		inst_hist.insert(inst_pnt);
 		switch (inst.op) {
-			case (Op::JMP):
+		case (Op::JMP):
 			inst_pnt += inst.value;
 			break;
-			case (Op::ACC):
+		case (Op::ACC):
 			acc_value += inst.value;
 			inst_pnt++;
 			break;
-			case (Op::NOP):
+		case (Op::NOP):
 			inst_pnt++;
 			break;
-			default:
+		default:
 			break;
 		}
 	}
 	return acc_value;
 }
 
-
-
 std::optional<int> repair(std::vector<Instruction> &inst_mem, unsigned max_count) {
-	auto i{0U};
+	auto i { 0U };
 	for (auto &inst : inst_mem) {
 		auto const old_inst = inst;
 		if (inst.op == Op::NOP) {
-			if(inst.value == 0){
+			if (inst.value == 0) {
 				continue;
 			}
-			inst.op=Op::JMP;
+			inst.op = Op::JMP;
 		} else if (inst.op == Op::JMP) {
-			inst.op=Op::NOP;
-		}else{
+			inst.op = Op::NOP;
+		} else {
 			continue;
 		}
 		auto result = interprete(inst_mem, false, max_count);
-		if(result){
+		if (result) {
 			//fmt::print("{}\n",i);
 			return result.value();
-		}else{
+		} else {
 			inst = old_inst;
 		}
 		i++;
@@ -121,12 +122,11 @@ std::optional<int> repair(std::vector<Instruction> &inst_mem, unsigned max_count
 	return {};
 }
 
-
-int repair_search(std::vector<Instruction> &inst_mem){
-	auto max_count{100U};
-	auto result { repair(inst_mem, max_count)};
-	while(not result){
-		max_count*=2;
+int repair_search(std::vector<Instruction> &inst_mem) {
+	auto max_count { 100U };
+	auto result { repair(inst_mem, max_count) };
+	while (not result) {
+		max_count *= 2;
 		result = repair(inst_mem, max_count);
 	}
 	return result.value();
@@ -134,7 +134,7 @@ int repair_search(std::vector<Instruction> &inst_mem){
 
 void tests() {
 	Instruction
-	inst {.op = Op::JMP, .value = 3};
+	inst { .op = Op::JMP, .value = 3 };
 	assert(parse_input_line("jmp +3") == inst);
 	inst.op = Op::ACC;
 	inst.value = -3;
@@ -144,12 +144,11 @@ void tests() {
 	assert(parse_input_line("nop +0") == inst);
 }
 
-
 int main() {
 	tests();
 	auto const *const file_name = "build/input/input_08.txt";
 	std::vector<Instruction> inst_mem = parse_input(file_name);
-	fmt::print("Part 1: {}\n",interprete(inst_mem,true,1000U).value());
-	fmt::print("Part 2: {}\n",repair_search(inst_mem));
+	fmt::print("Part 1: {}\n", interprete(inst_mem, true, 1000U).value());
+	fmt::print("Part 2: {}\n", repair_search(inst_mem));
 	return EXIT_SUCCESS;
 }
