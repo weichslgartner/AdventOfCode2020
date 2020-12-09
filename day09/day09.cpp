92b898d32c6380e405f2d2a5f7a20bf98811cf9b
weichslgartner <weichslgartner@gmail.com>
2020-12-09 11:22:48 +0100

    refactor day09

--- day09/day09.cpp
+++ day09/day09.cpp
@@ -9,8 +9,6 @@
 #include <deque>
 #include <numeric>
 
-
-
 std::vector<unsigned long long> parse_input(char const *const file_name) {
 	std::ifstream infile(file_name);
 	if (!infile.is_open()) {
@@ -22,20 +20,15 @@ std::vector<unsigned long long> parse_input(char const *const file_name) {
 	ss << infile.rdbuf();
 	std::vector<unsigned long long> numbers;
 	while (std::getline(ss, line)) {
-		//fmt::print("{}\n",std::stoi(line));
 		numbers.push_back(std::stoull(line));
 	}
 	return numbers;
 }
-
-int main() {
-	auto const *const file_name = "build/input/input_09.txt";
-	auto numbers = parse_input(file_name);
-	std::deque<unsigned long long> deq;
-	std::unordered_set<unsigned long long> num_mem;
-	constexpr auto prelog_size { 25U };
+template<typename T>
+auto part_1(std::vector<T> const &numbers, T prelog_size) {
 	auto target { 0ULL };
-	auto part2 { 0ULL };
+	std::deque<T> deq;
+	std::unordered_set<T> num_mem;
 	for (auto const number : numbers) {
 		if (deq.size() < prelog_size) {
 			deq.push_back(number);
@@ -49,23 +42,26 @@ int main() {
 					break;
 				}
 			}
-			if (not found) {
-				//fmt::print("not found {}\n", number);
+			if (!found) {
 				target = number;
 				break;
 			}
-
 			num_mem.erase(deq.front());
 			num_mem.insert(number);
 			deq.pop_front();
 			deq.push_back(number);
 		}
 	}
-	std::vector<unsigned long long> window;
-	window.reserve(prelog_size);
+	return target;
+}
+
+template<typename T>
+auto part_2(std::vector<T> const &numbers, T target) {
+	auto part2 { 0ULL };
+	std::vector<T> window { };
 	for (int i { 0 }; i < numbers.size(); ++i) {
 		auto j { i };
-		while (std::accumulate(window.begin(), window.end(), 0ULL) < target and j < numbers.size()) {
+		while (std::accumulate(window.begin(), window.end(), 0ULL) < target && j < numbers.size()) {
 			window.push_back(numbers[j]);
 			++j;
 		}
@@ -77,6 +73,16 @@ int main() {
 			window.clear();
 		}
 	}
+	return part2;
+}
+
+int main() {
+	auto const *const file_name = "build/input/input_09.txt";
+	auto numbers = parse_input(file_name);
+
+	constexpr auto prelog_size { 25U };
+	auto target = part_1<unsigned long long>(numbers, prelog_size);
+	auto part2 = part_2<unsigned long long>(numbers, target);
 	fmt::print("Part 1: {}\n", target);
 	fmt::print("Part 2: {}\n", part2);
 	return EXIT_SUCCESS;
