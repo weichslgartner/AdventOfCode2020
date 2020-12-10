d99c382f8d75e9bf5c0741b0b67831163eac361f
weichslgartner <weichslgartner@gmail.com>
2020-12-10 13:35:02 +0100

    template refactoring

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -30,10 +30,28 @@ std::vector<T> parse_input(char const *const file_name) {
 	return numbers;
 }
 
-unsigned long long dfs(int const node, int const max, std::unordered_map<int, std::vector<int>> const &dag,
-		std::unordered_map<int, unsigned long long> &paths) {
+
+template<Integral T>
+std::unordered_map<T, std::vector<T> > parse_dag(std::vector<T> const &numbers, std::unordered_set<T> const &set) {
+	std::unordered_map<T, std::vector<T> > dag { };
+	for (auto jolt : numbers) {
+		for (int i { 1 }; i <= 3; ++i) {
+			if (set.contains(jolt + i)) {
+				if (!dag.contains(jolt)) {
+					dag[jolt] = std::vector<T>();
+				}
+				dag[jolt].push_back(jolt + i);
+			}
+		}
+	}
+	return dag;
+}
+
+template<Integral T, Integral Big>
+Big dfs(T const node, T const max, std::unordered_map<T, std::vector<T>> const &dag,
+		std::unordered_map<T, Big> &paths) {
 	if (node == max) {
-		return 1;
+		return Big{1};
 	} else {
 		if (not paths.contains(node)) {
 			auto sum { 0ULL };
@@ -46,7 +64,8 @@ unsigned long long dfs(int const node, int const max, std::unordered_map<int, st
 	}
 }
 
-auto part_1(int const max, std::unordered_set<int> const &set) {
+template<Integral T>
+auto part_1(int const max, std::unordered_set<T> const &set) {
 	auto cur_jolt { 0 };
 	auto one_diff { 0 };
 	auto three_diff { 0 };
@@ -62,45 +81,23 @@ auto part_1(int const max, std::unordered_set<int> const &set) {
 	return one_diff * three_diff;
 }
 
-std::unordered_map<int, std::vector<int> > parse_dag(std::vector<int> const &numbers, std::unordered_set<int> const &set) {
-	std::unordered_map<int, std::vector<int> > dag { };
-	for (auto jolt : numbers) {
-		if (set.contains(jolt + 1)) {
-			if (!dag.contains(jolt)) {
-				dag[jolt] = std::vector<int>();
-			}
-			dag[jolt].push_back(jolt + 1);
-		}
-		if (set.contains(jolt + 2)) {
-			if (!dag.contains(jolt)) {
-				dag[jolt] = std::vector<int>();
-			}
-			dag[jolt].push_back(jolt + 2);
-		}
-		if (set.contains(jolt + 3)) {
-			if (!dag.contains(jolt)) {
-				dag[jolt] = std::vector<int>();
-			}
-			dag[jolt].push_back(jolt + 3);
-		}
-	}
-	return dag;
-}
+
 
 int main() {
 	auto const *const file_name = "build/input/input_10.txt";
 	using T = int;
+	using Big = unsigned long long;
 	auto numbers = parse_input<T>(file_name);
 	auto max = *std::max_element(numbers.begin(), numbers.end());
 	numbers.push_back(max + 3);
 	numbers.push_back(0);
-	std::unordered_set<int> set { };
+	std::unordered_set<T> set { };
 	set.insert(numbers.begin(), numbers.end());
-	auto res1 = part_1(max, set);
-	fmt::print("Part 1: {}\n",res1 );
-	auto dag = parse_dag(numbers, set);
-	std::unordered_map<int, unsigned long long> pathmap { };
-	auto paths = dfs(0,max+3,dag,pathmap);
+	auto res1 = part_1<T>(max, set);
+	fmt::print("Part 1: {}\n", res1);
+	auto dag = parse_dag<T>(numbers, set);
+	std::unordered_map<T,Big> pathmap { };
+	auto paths = dfs(0, max + 3, dag, pathmap);
 	fmt::print("Part 2: {}\n", paths);
 	return EXIT_SUCCESS;
 }
