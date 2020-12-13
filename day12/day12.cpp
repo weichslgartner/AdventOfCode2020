7c3381facf86877013326c2a0c61809c9d9308e8
weichslgartner <weichslgartner@gmail.com>
2020-12-13 21:15:41 +0100

    solution day12

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -1,17 +1,9 @@
 #include "common.h"
-#include <algorithm>
-#include <array>
-#include <assert.h>
-#include <deque>
+#include <cassert>
 #include <fmt/format.h>
 #include <fstream>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <regex>
 #include <sstream>
 #include <string>
-#include <tuple>
 #include <vector>
 
 enum class Cardinals {
@@ -36,14 +28,12 @@ std::vector<NavInst> parse_instructions(char const *file_name) {
 	while (std::getline(ss, line)) {
 		auto dir = line[0];
 		auto val = std::stoul(line.substr(1, line.size() - 1));
-		fmt::print("{} {}\n", dir, val);
 		ints.emplace_back(NavInst { dir, val });
-
 	}
 	return ints;
 }
 
-Point card2coord(Cardinals card) {
+Point card2coord(Cardinals const card) {
 	switch (card) {
 	case Cardinals::EAST:
 
@@ -58,10 +48,10 @@ Point card2coord(Cardinals card) {
 
 	}
 	//never reached
-	return{};
+	return {};
 }
 
-Point& move(Point &point, char action, Cardinals card, int distance) {
+Point& move(Point &point, char const action, Cardinals const card, int const distance) {
 	auto next = card2coord(card);
 	switch (action) {
 	case 'F':
@@ -88,11 +78,11 @@ Point& move(Point &point, char action, Cardinals card, int distance) {
 	return point;
 }
 
-Cardinals turn(Cardinals cur_dir, char turn_dir, int degree) {
+Cardinals turn(Cardinals const cur_dir, char turn_dir, int degree) {
 	if (degree % 90 != 0) {
 		fmt::print(stderr, "degree not multiple of 90 (given {})\n", degree);
 	}
-	auto times = degree / 90;
+	auto const times = degree / 90;
 	auto cur_dir_int = static_cast<int>(cur_dir);
 	switch (turn_dir) {
 	case 'R':
@@ -111,26 +101,8 @@ Cardinals turn(Cardinals cur_dir, char turn_dir, int degree) {
 	return static_cast<Cardinals>(cur_dir_int);
 }
 
-void tests() {
-	assert(turn(Cardinals::EAST, 'R', 90) == Cardinals::SOUTH);
-	assert(turn(Cardinals::EAST, 'L', 90) == Cardinals::NORTH);
-	assert(turn(Cardinals::EAST, 'L', 180) == Cardinals::WEST);
-	assert(turn(Cardinals::EAST, 'R', 180) == Cardinals::WEST);
-	assert(turn(Cardinals::SOUTH, 'R', 270) == Cardinals::EAST);
-	assert(turn(Cardinals::SOUTH, 'L', 270) == Cardinals::WEST);
-	Point p{0,0};
-	auto card = Cardinals::EAST;
-	auto res = move(p, 'F', card, 10);
-	assert(res.x==10 and res.y==0);
-	res = move(res, 'N', card, 3);
-	assert(res.x==10 and res.y==3);
-}
 
-int main() {
-	constexpr auto file_name = "build/input/input_12.txt";
-	tests();
-	auto instructions = parse_instructions(file_name);
-	Point p { 0, 0 };
+Point part_1(std::vector<NavInst, std::allocator<NavInst> > const &instructions, Point p) {
 	Cardinals cur_dir { Cardinals::EAST };
 	for (auto inst : instructions) {
 		switch (inst.dir) {
@@ -139,12 +111,105 @@ int main() {
 			cur_dir = turn(cur_dir, inst.dir, inst.value);
 			break;
 		default:
-			p =  move(p, inst.dir, cur_dir, inst.value);
+			p = move(p, inst.dir, cur_dir, inst.value);
 			break;
 		}
 	}
-	//print_grid(grid);
-	fmt::print("Part 1: {}\n", p.manhattan_distance());
-	fmt::print("Part 2: {}\n", "");
+	return p;
+}
+
+Point& rotate_waypoint(Point &p, NavInst inst) {
+	switch (inst.dir) {
+	case 'R':
+		switch (inst.value) {
+		case 90:
+			p = Point { p.y, -p.x };
+			break;
+		case 180:
+			p = Point { -p.x, -p.y };
+			break;
+		case 270:
+			p = Point { -p.y, p.x };
+			break;
+		default:
+			fmt::print(stderr, "unkown degree");
+		}
+		break;
+	case 'L':
+		switch (inst.value) {
+		case 90:
+			p = Point { -p.y, p.x };
+			break;
+		case 180:
+			p = Point { -p.x, -p.y };
+			break;
+		case 270:
+			p = Point { p.y, -p.x };
+			break;
+		default:
+			fmt::print(stderr, "unkown degree");
+		}
+		break;
+	}
+	return p;
+}
+
+Point part2(std::vector<NavInst, std::allocator<NavInst> > const &instructions, Point p, Point waypoint) {
+	for (auto inst : instructions) {
+		switch (inst.dir) {
+		case 'F':
+			p.x += inst.value * waypoint.x;
+			p.y += inst.value * waypoint.y;
+			break;
+		case 'N':
+			waypoint.y += inst.value;
+			break;
+		case 'S':
+			waypoint.y -= inst.value;
+			break;
+		case 'W':
+			waypoint.x -= inst.value;
+			break;
+		case 'E':
+			waypoint.x += inst.value;
+			break;
+		case 'R':
+		case 'L':
+			waypoint = rotate_waypoint(waypoint, inst);
+			break;
+		default:
+			break;
+		}
+	}
+	return p;
+}
+
+void tests() {
+	assert(turn(Cardinals::EAST, 'R', 90) == Cardinals::SOUTH);
+	assert(turn(Cardinals::EAST, 'L', 90) == Cardinals::NORTH);
+	assert(turn(Cardinals::EAST, 'L', 180) == Cardinals::WEST);
+	assert(turn(Cardinals::EAST, 'R', 180) == Cardinals::WEST);
+	assert(turn(Cardinals::SOUTH, 'R', 270) == Cardinals::EAST);
+	assert(turn(Cardinals::SOUTH, 'L', 270) == Cardinals::WEST);
+	Point p { 0, 0 };
+	auto card = Cardinals::EAST;
+	auto res = move(p, 'F', card, 10);
+	assert((res.x == 10) and (res.y == 0));
+	res = move(res, 'N', card, 3);
+	assert((res.x == 10) and (res.y == 3));
+}
+
+
+int main() {
+	constexpr auto file_name = "build/input/input_12.txt";
+	tests();
+	auto instructions = parse_instructions(file_name);
+	Point p { 0, 0 };
+	p = part_1(instructions, p);
+	fmt::print("Part 1: {}\n",  p.manhattan_distance(Point { 0, 0 }));
+	p = Point{ 0, 0 };
+	Point waypoint { 10, 1 };
+	p = part2(instructions, p, waypoint);
+	fmt::print("Part 2: {}\n", p.manhattan_distance(Point { 0, 0 }));
 	return EXIT_SUCCESS;
 }
