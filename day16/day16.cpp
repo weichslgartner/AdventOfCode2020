5a012045d4add41058e9cbb4bc75584d661e781e
weichslgartner <weichslgartner@gmail.com>
2020-12-16 17:46:47 +0100

    day16 part2

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -16,6 +16,7 @@
 #include <unordered_map>
 #include <utility>
 #include <tuple>
+#include <unordered_set>
 #include <vector>
 
 struct ValidRange {
@@ -72,19 +73,22 @@ int main() {
 	constexpr auto file_name = "build/input/input_16.txt";
 	auto [rules, your_ticket, nearby_tickets] = parse_input(file_name);
 	auto error_rate { 0U };
-	auto numb_rules{12};
+	auto numb_rules { 12 };
 	auto departure_rules = std::vector<ValidRange>(rules.begin(), rules.begin() + numb_rules);
+	auto all_rules = rules;
 	std::sort(rules.begin(), rules.end());
-	for (auto rule : departure_rules) {
-		fmt::print("{} {}\n", rule.min, rule.max);
+	auto cnt{0};
+	for (auto rule : all_rules) {
+		fmt::print("{} {} {}\n",cnt, rule.min, rule.max);
+		cnt+=1;
 	}
 	auto ticket_cnt { 0U };
 	auto invalid_cnt { 0U };
-	std::vector<std::vector<int>> valid_tickets{};
+	std::vector<std::vector<int>> valid_tickets { };
 
 	for (auto ticket : nearby_tickets) {
 		auto i { 0U };
-		auto isTicketValid{true};
+		auto isTicketValid { true };
 		for (auto number : ticket) {
 			//fmt::print("number {} \n",number);
 			bool isFieldValid { false };
@@ -109,15 +113,14 @@ int main() {
 			i++;
 
 		}
-		if(isTicketValid){
+		if (isTicketValid) {
 			valid_tickets.push_back(ticket);
 			//fmt::print(" valid {}\n", ticket_cnt);
-		}else{
+		} else {
 			//fmt::print("NOT valid {}\n", ticket_cnt);
 
 		}
 		ticket_cnt++;
-
 
 	}
 	std::unordered_map<int, std::unordered_map<int, int>> index_map { };
@@ -126,13 +129,12 @@ int main() {
 		auto i { 0U };
 		for (auto number : ticket) {
 			auto departure_rule_idx_raw { 0U };
-			for (auto rule : departure_rules) {
+			for (auto rule : all_rules) {
 				if (number >= rule.min and number <= rule.max) {
 					auto departure_rule_idx = departure_rule_idx_raw;
-					if(departure_rule_idx_raw % 2 != 0){
+					if (departure_rule_idx_raw % 2 != 0) {
 						departure_rule_idx--;
 					}
-
 
 					if (not index_map.contains(i)) {
 						index_map.insert( { i, std::unordered_map<int, int>() });
@@ -151,32 +153,74 @@ int main() {
 
 	fmt::print("Valid tickets: {}\n", valid_tickets.size());
 	fmt::print("Part 1: {}\n", error_rate);
-	std::unordered_set<int> unique_ids;
+	std::unordered_map<int,std::unordered_set<int>> unique_ids;
+
 	for (auto el : index_map) {
 		fmt::print("{}: ", el.first);
 		//auto unique_ids = el.second;
 		//std::sort(el.second.begin(),el.second.end());
 		for (auto occ : el.second) {
-			fmt::print("{},{}| ", occ.first, occ.second);
-			if( occ.second == valid_tickets.size()){
-				unique_ids.insert(el.first);
+
+			if (occ.second == valid_tickets.size()) {
+			//	unique_ids.insert(el.first);
+				fmt::print("{},{} size {}| ", occ.first, occ.second,el.second.size());
+				if(not unique_ids.contains(el.first)){
+					unique_ids[el.first] = std::unordered_set<int>();
+				}
+				unique_ids[el.first].insert( occ.first);
+			}else{
+				//el.second.erase(occ.first);
 			}
-			/*
-			for (auto el2 : index_map) {
-				if (el.first == el2.first) {
-					continue;
-				}
-				if (el2.second.contains(occ.first)) {
-					unique_ids.erase(occ.first);
-				}
-			}*/
+
 		}
 		fmt::print("\n");
 
 	}
-	for (auto id : unique_ids) {
-				fmt::print("{}\n", id);
+	auto part2{1ULL};
+	auto found{0};
+	while(found <20 ){
+		auto i{0};
+		for (auto &el : unique_ids) {
+			if(el.second.size() == 1){
+				auto to_erase = *el.second.begin();
+				fmt::print("{}->{}\n ", el.first,to_erase);
+				if(to_erase <= 10){
+					part2 *= static_cast<unsigned long long>(your_ticket.at(el.first));
+				}
+				found++;
+				for (auto &u : unique_ids) {
+					u.second.erase(to_erase);
+					//fmt::print("erased {}->{}\n ", el.first,to_erase);
+
+				}
+
 			}
+			i++;
+		}
+	}
+	fmt::print("your ticket  \n");
+	for (auto number : your_ticket) {
+		fmt::print(" {} \n", number);
+	}
+
+	fmt::print("\n part2 {} \n", part2);
+	auto i { 0 };
+	for (auto number : your_ticket) {
+		auto departure_rule_idx_raw { 0U };
+		for (auto rule : departure_rules) {
+			if (number >= rule.min and number <= rule.max) {
+				auto departure_rule_idx = departure_rule_idx_raw;
+				if (departure_rule_idx_raw % 2 != 0) {
+					departure_rule_idx--;
+				}
+				if (unique_ids.contains(i)) {
+					//fmt::print("matched {} {}\n", i,departure_rule_idx);
+				}
+			}
+			departure_rule_idx_raw++;
+		}
+		i++;
+	}
 
 	return EXIT_SUCCESS;
 }
